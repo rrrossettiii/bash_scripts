641331d7a35a0b2879c6ed55c78c3d44b7b79af4
Dante Ivec <rrrossettiii@gmail.com>
2023-05-19 00:15:33 -0400

    commented extra packages in init.sh

--- .bash_scripts/init.sh
+++ .bash_scripts/init.sh
@@ -8,12 +8,12 @@ load-all-scripts(){
     local SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
     ### _================
-    ### __Load Order [comment out what you dont need]
+    ### __Load Order [uncomment what you need]
     ### _================
     for __bs_file in $SCRIPT_DIR/bash/*; do source $__bs_file;done #_(0): all "bash" scripts
-    for __bs_file in $SCRIPT_DIR/packages/*; do source $__bs_file;done #_(1): "package" scripts
-    for __bs_file in $SCRIPT_DIR/linux/*; do source $__bs_file;done #_(2): "linux" scripts
-    for __bs_file in $SCRIPT_DIR/arch/*; do source $__bs_file;done #_(3): "arch" scripts
+    # for __bs_file in $SCRIPT_DIR/packages/*; do source $__bs_file;done #_(1): "package" scripts
+    # for __bs_file in $SCRIPT_DIR/linux/*; do source $__bs_file;done #_(2): "linux" scripts
+    # for __bs_file in $SCRIPT_DIR/arch/*; do source $__bs_file;done #_(3): "arch" scripts
 }
 ### _(run fn)
 load-all-scripts;
