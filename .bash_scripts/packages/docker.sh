aad0fce4d92bad0e66610ee4a4de07f8ef73c594
Dante Ivec <rrrossettiii@gmail.com>
2023-05-21 17:29:01 -0400

    docker script updates

--- .bash_scripts/packages/docker.sh
+++ .bash_scripts/packages/docker.sh
@@ -33,16 +33,16 @@ fleet(){ ### create '/fleet' /dir in $DOCKER_COMPOSE_PATH ($ `mkdir $DOCKER_COMP
     dock;
     local FLEET_FILES=($(for f in fleet/*; do printf -- "--file\n$f\n"; done)) ### grab file names & add flag(s)
     case $1 in
-        sail)   ### launch all projects
+        sail) ### launch all projects
             sudo docker compose "${FLEET_FILES[@]}" up -d --remove-orphans;;
-        capsize)
+        capsize) ### remove all projects
             sudo docker compose "${FLEET_FILES[@]}" down;;
-        port)   ### update all projects
+        port) ### update all projects
             sudo docker compose "${FLEET_FILES[@]}" down;
             sudo docker compose "${FLEET_FILES[@]}" pull;
             sudo docker compose "${FLEET_FILES[@]}" up -d --remove-orphans;
             ;;
         *)  ### print fleet files
-            test-array FLEET_FILES;;
+            sudo docker compose ls;;
     esac
 }
